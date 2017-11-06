57e1583b1365f9f332e11819065d2253951cf63a
lijuntao <lijuntao1@jd.com>
2017-11-06 15:21:58 +0800

    糗事百科界面抓取段子,每点击一次enter按键出一条段子,一直获取,知道输入Ｑ进行退出

--- com.anyMarvel.python/qiushibaike.py
+++ com.anyMarvel.python/qiushibaike.py
@@ -4,7 +4,7 @@ import re
 
 page = 1
 url = 'http://www.qiushibaike.com/hot/page/' + str(page)
-user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36'
 headers = {'User-Agent': user_agent}
 try:
     request = urllib2.Request(url, headers=headers)
